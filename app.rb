beb2c19314bd812b9d332b3bc9933b72fcf9023a
jessie5557 <ychen38@chicagobooth.edu>
2025-02-05 03:38:39 +0000

    random

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get("/") do
   "
 end
 
+#square
 get("/square/new") do
   erb(:new_square_calc)
 end
@@ -19,3 +20,32 @@ get("/square/results") do
 
   erb(:square_results)
 end
+
+#square root
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num **0.5
+
+  erb(:square_root_results)
+end
+
+#random
+get("/random/new") do
+  erb(:new_random)
+end
+
+get("/random/results") do
+
+  @the_min_num = params.fetch("users_min_number").to_i
+  @the_max_num = params.fetch("users_max_number").to_i
+  @the_result = rand(@the_min_num..@the_max_num)
+
+  erb(:square_root_results)
+end
+
+#payment
